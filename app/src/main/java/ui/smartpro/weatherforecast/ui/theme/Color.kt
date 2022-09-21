3931bd4f795526454ba56cc5c261977661e52583
artem_inkaz <artem.inkaz@gmail.com>
2022-09-21 11:14:24 +0600

    [ДОБАВЛЕНО] colors в ресурсы compose

--- app/src/main/java/ui/smartpro/weatherforecast/ui/theme/Color.kt
+++ app/src/main/java/ui/smartpro/weatherforecast/ui/theme/Color.kt
@@ -6,3 +6,9 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//UI colors
+val firstGrayBlue = Color(0xFF9DE44D)
+val secondOrangeDawn = Color(0xFFFFEB3B)
+val secondaryLightCarrot = Color(0xFFFFA957)
+val secondaryPearlWhite = Color(0xFFEDE6CB)
